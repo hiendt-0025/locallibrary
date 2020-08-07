c8186cbdeb0fc53f0ef7c84026445f812e5ce36b
hiendt-0025 <dao.thi.hien@sun-asterisk.com>
2020-08-07 16:45:38 +0700

    Django Tutorial Part 4: Django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+# admin.site.register(Genre)
+# admin.site.register(Language)
+
+class BooksInline(admin.TabularInline):
+  """docstring for BooksInstanceInline"""
+  model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+  fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+  inlines = [BooksInline]
+
+class BooksInstanceInline(admin.TabularInline):
+  """docstring for BooksInstanceInline"""
+  model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('title', 'author', 'display_genre')
+  inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+  list_display = ('book', 'imprint', 'due_back', 'status', 'id')
+  list_filter = ('status', 'due_back')
+
+  fieldsets = (
+    (None, {
+      'fields': ('book', 'imprint', 'id')
+    }),
+    ('Availability', {
+      'fields': ('status', 'due_back')
+    }),
+  )
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+  """docstring for Genre"""
+  pass
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+  """docstring for Genre"""
+  pass
