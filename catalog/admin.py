73bba0a96c62b78bdf7070ed574fd048c9b68e08
hiendt-0025 <dao.thi.hien@sun-asterisk.com>
2020-08-10 10:20:56 +0700

    Django Tutorial Part 5: Creating our home page

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-# admin.site.register(Genre)
-# admin.site.register(Language)
-
 class BooksInline(admin.TabularInline):
   """docstring for BooksInstanceInline"""
   model = Book
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre, Language
 
-# Create your views here.
+def index(request):
+  num_books = Book.objects.all().count()
+  num_instances = BookInstance.objects.all().count()
+
+  # Available books (status = 'a')
+  num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+  num_authors = Author.objects.count()
+
+  num_genre = Genre.objects.count()
+
+  num_books_available = Book.objects.filter(title__contains='Nhung').count()
+
+  context = {
+    'num_books': num_books,
+    'num_instances': num_instances,
+    'num_instances_available': num_instances_available,
+    'num_authors': num_authors,
+    'num_genre': num_genre,
+    'num_books_available': num_books_available,
+  }
+
+  return render(request, 'index.html', context = context)
